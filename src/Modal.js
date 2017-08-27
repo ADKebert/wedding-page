46deaa5660dd0da1010cc2a8966692d24f21c97d
Alan Kebert <adkebert@yahoo.com>
2017-08-27 17:33:29 -0400

    made the modals responsive

--- src/Modal.js
+++ src/Modal.js
@@ -2,34 +2,10 @@ import React, { Component } from 'react';
 
 class Modal extends Component {
   render() {
-    let modalStyle = {
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      zIndex: '9999',
-      background: '#fcfcf5',
-      minHeight: '50vh',
-      maxHeight: '100vh',
-      minWidth: '40vw',
-      overflowY: 'scroll',
-      padding: '0px 20px 0px',
-    }
-
-    let backdropStyle = {
-      position: 'absolute',
-      width: '100%',
-      height: '100%',
-      top: '0px',
-      left: '0px',
-      zIndex: '9998',
-      background: 'rgba(0, 0, 0, 0.3)'
-    }
-
     return (
       <div>
-        <div style={modalStyle}>{this.props.children}</div>
-        <div style={backdropStyle} onClick={e => this.close(e)}/>
+        <div className='modal'>{this.props.children}</div>
+        <div className='backdrop' onClick={e => this.close(e)}/>
       </div>
     )
   }
